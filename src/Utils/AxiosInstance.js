c3f6afab3d02eddf9e909eaa1f5467131d29979e
adarsh6672 <adarshkeruvatty@gmail.com>
2024-06-07 14:54:33 +0530

    new host name

--- src/Utils/AxiosInstance.js
+++ src/Utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const AxiosInstance=axios.create({
-    baseURL:'http://127.0.0.1:8080/',
+    baseURL:'http://gateway-service:8080/',
     'Content-type':'application/json',
 })
 
--- src/Utils/const.js
+++ src/Utils/const.js
@@ -1,3 +1,3 @@
-export const BASE_URL= "http://localhost:8080"
+export const BASE_URL= "http://gateway-service:8080"
 export const RAZORPAY_API_KEY= "rzp_test_8b6GHpvbMnFOEM"
 export const RAZORPAY_API_SECRET= "meOU2C0AW0axCyREWHQYZ1Qf"
