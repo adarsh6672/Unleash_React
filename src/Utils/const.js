8711739bd45f6630b8a82ea4bd7b5f44e037c4e4
adarsh6672 <adarshkeruvatty@gmail.com>
2024-06-08 18:49:54 +0530

    added api url

--- src/Utils/const.js
+++ src/Utils/const.js
@@ -1,3 +1,5 @@
-export const BASE_URL= "http://localhost:8080/api"
+//export const BASE_URL= "http://localhost:8080/api"
+export const BASE_URL= "http://unleashwell.online/api"
+
 export const RAZORPAY_API_KEY= "rzp_test_8b6GHpvbMnFOEM"
 export const RAZORPAY_API_SECRET= "meOU2C0AW0axCyREWHQYZ1Qf"
